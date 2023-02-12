3f8845aec976f8ef231d9bc3cecea63be245f4f6
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-02-12 01:55:26 -0600

    learning to work with objects

---
+++ accessing_objects.js
@@ -0,0 +1,26 @@
+// dot notation
+
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;
+const shirtValue = testObj.shirt;
+
+
+// bracket notation
+
+// Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];
+const drinkValue = testObj["the drink"];
---
+++ objects.js
@@ -0,0 +1,11 @@
+const myDog = {
+    // Only change code below this line
+    "name": "asha",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["kai", "bear"]
+
+    // Only change code above this line
+};
+
+// console.log(myDog);
