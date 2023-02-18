a41127bee6ce343ec4b81958f7e67641b74b2b75
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-02-18 17:35:27 -0600

    learning how to access nested arrays

---
+++ access_nested_array.js
@@ -0,0 +1,22 @@
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = myPlants[1].list[1];
+const firstFlower = myPlants[0].list[0];
+const lastTree = myPlants[1].list[2];
