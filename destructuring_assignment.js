3bfcf63dd09a891a5c22551867bbdd8120ce5f00
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-02-19 19:52:35 -0600

    learning destructuring assignment to assign variable from objects

---
+++ destructuring_assignment.js
@@ -0,0 +1,11 @@
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+
+// const highToday = HIGH_TEMPERATURES.today;
+// const highTomorrow = HIGH_TEMPERATURES.tomorrow;
+
+const { highToday: today, highTomorrow: tomorrow } = HIGH_TEMPERATURES;
