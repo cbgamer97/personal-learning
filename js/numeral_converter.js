c643ee6485854f70462130cfcefed61200703e87
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-08-16 23:34:07 -0500

    more leet code practice

---
+++ js/numeral_converter.js
@@ -0,0 +1,30 @@
+function convertToRoman(num) {
+
+    let roman = "";
+    const romanNumeral = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+
+    for (let key in romanNumeral) {
+        while (num >= romanNumeral[key]) {
+            roman += key;
+            num -= romanNumeral[key];
+        }
+    }
+
+    return roman;
+}
+
+convertToRoman(36);
